1715fb856bcde885ad1420dd49862b6b6f879b16
Eric Souza Romero <eriicsouzaromeroo@gmail.com>
2024-03-13 02:20:29 -0300

    Criado rotas e service de post

---
+++ pages/api/post/index.js
@@ -0,0 +1,36 @@
+import {withIronSessionApiRoute} from 'iron-session/next'
+
+import createHandler from "../../../lib/middleware/nextConnect";
+import validation from "../../../lib/middleware/validation";
+
+import { ironConfig } from "../../../lib/middleware/ironSession";
+
+import { createPostSchema } from '../../../modules/post/post.schema';
+import { createPost, getPosts } from '../../../modules/post/post.service';
+
+const handler = createHandler()
+
+handler
+.post(validation({body: createPostSchema}), async(req, res) =>{
+    try{
+        if(!req.session.user) return res.status(401).send()
+
+        const newPost = await createPost(req.body, req.session.user)
+        res.status(201).send(newPost)
+    } catch(err){
+        return res.status(500).send(err.message)
+    }
+})
+.get(async(req, res) => {
+    try{
+        if(!req.session.user) return res.status(401).send()
+
+        const posts = await getPosts()
+        res.status(200).send(posts)
+    } catch(err){
+        return res.status(500).send(err.message)
+    }
+})
+
+
+export default withIronSessionApiRoute(handler, ironConfig)
