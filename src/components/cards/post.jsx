ab1f6bc856ce2e8542fb2215eaefd8fe4500bad8
Eric Souza Romero <eriicsouzaromeroo@gmail.com>
2024-03-14 14:18:22 -0300

    passado destructor para implementação de dados automaticamente

--- src/components/cards/post.jsx
+++ src/components/cards/post.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import moment from 'moment'
 
 const PostContainer = styled.div`
     background-color: ${props=>props.theme.white};
@@ -19,13 +20,13 @@ const ContainerText = styled.div`
     margin-top:18px;
 `
 
-function Post(){
+function Post({text, user, date}){
     return(
         <PostContainer>
-            <StyledUsername> @Username </StyledUsername>
-            <StyledDate> 22 de setembro 2004</StyledDate>
+            <StyledUsername> @{user}</StyledUsername>
+            <StyledDate> {moment(date).format('LLL')}</StyledDate>
             <ContainerText>
-                Texto de teste
+                {text}
             </ContainerText>
         </PostContainer>
     )
